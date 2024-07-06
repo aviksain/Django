8b3942717b369886d8dbe3b2a97f8864778a3742
avik <aviksain007@gmail.com>
2024-07-06 14:19:51 +0530

    done jinja

--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 def home(request):
   # return HttpResponse("<h1>This is our Home Page</h1>")
-  return render(request, 'layout.html')
+  return render(request, 'website/home.html')
 
 def about(request):
-  return HttpResponse("<h1>This is our About Page</h1>")
+  # return HttpResponse("<h1>This is our About Page</h1>")
+  return render(request, 'website/about.html')
 
 def contact(request):
-  return HttpResponse("<h1>This is our Contact Page</h1>")
+  # return HttpResponse("<h1>This is our Contact Page</h1>")
+  return render(request, 'website/contact.html')
