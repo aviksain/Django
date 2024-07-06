4848bd60064c5894587ea248706fb084c496fc02
avik <aviksain007@gmail.com>
2024-07-07 00:17:53 +0530

    tailwind integration done

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('app/', include('myapp.urls'))
+    path('app/', include('myapp.urls')),
+    path("__reload__/", include("django_browser_reload.urls"))
 ]
